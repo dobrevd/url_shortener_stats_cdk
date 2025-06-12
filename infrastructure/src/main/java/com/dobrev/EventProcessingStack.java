503d2c99e9096f7776aed331957b3241f8b23c9f
dobrevd <dobrev@ukr.net>
2025-06-12 18:28:11 +0200

    add S3 to EventProcessingStack

--- infrastructure/src/main/java/com/dobrev/EventProcessingStack.java
+++ infrastructure/src/main/java/com/dobrev/EventProcessingStack.java
@@ -15,12 +15,17 @@ import software.amazon.awscdk.services.events.CronOptions;
 import software.amazon.awscdk.services.events.RuleProps;
 import software.amazon.awscdk.services.events.Schedule;
 import software.amazon.awscdk.services.events.targets.LambdaFunction;
+import software.amazon.awscdk.services.iam.PolicyStatement;
 import software.amazon.awscdk.services.lambda.Code;
 import software.amazon.awscdk.services.lambda.Function;
 import software.amazon.awscdk.services.lambda.FunctionProps;
 import software.amazon.awscdk.services.lambda.Runtime;
 import software.amazon.awscdk.services.events.Rule;
+import software.amazon.awscdk.services.s3.Bucket;
+import software.amazon.awscdk.services.s3.BucketProps;
+import software.amazon.awscdk.services.s3.LifecycleRule;
 
+import java.util.Collections;
 import java.util.List;
 
 
@@ -69,12 +74,30 @@ public class EventProcessingStack extends Stack {
                         .build())
                 .build());
 
+        Bucket bucket = new Bucket(this, "DeletedUrlEvents", BucketProps.builder()
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .autoDeleteObjects(true)
+                .lifecycleRules(Collections.singletonList(LifecycleRule.builder()
+                        .enabled(true)
+                        .expiration(Duration.seconds(1))
+                        .build()))
+                .build());
+
         Function lambda = new Function(this, "Lambda", FunctionProps.builder()
                 .runtime(Runtime.JAVA_17)
                 .handler("com.dobrev.handler::clean;")
                 .code(Code.fromAsset("lambda"))
                 .memorySize(512)
                 .timeout(Duration.seconds(15))
+                .build());
+        lambda.addToRolePolicy(PolicyStatement.Builder.create()
+                .actions(List.of("dynamodb:GetItem", "dynamodb:Query", "dynamodb:Scan", "dynamodb:DeleteItem"))
+                .resources(List.of(urlEventsDdb.getTableArn(),
+                        urlEventsDdb.getTableArn() + "/index/*"))
+                .build());
+        lambda.addToRolePolicy(PolicyStatement.Builder.create()
+                .actions(List.of("s3:PutObject"))
+                .resources(List.of(bucket.getBucketArn() + "/*"))
                 .build());
 
         Rule rule = new Rule(this, "CleanupScheduleRule", RuleProps.builder()
